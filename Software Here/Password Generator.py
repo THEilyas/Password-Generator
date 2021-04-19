0bce362ab8d6fffd7dd3e0dbf27f8ad81f455b6f
ilyasYILDIZ <62676225+THEilyas@users.noreply.github.com>
2021-04-19 05:41:26 +0300

    Password Generator.py
    Version : 1.0.0
    
    This software is freeware.
    
    It is not sold for money.
    
    It is open source and anyone can develop it.

--- Software Here/Password Generator.py
+++ Software Here/Password Generator.py
@@ -1,6 +1,9 @@
 #Modules
+import time
 import random
 #Variables
+ #boolean
+rgt = random.getrandbits(1)
 tf = random.random()
  #Password Character lenght
 pcl = 1
@@ -14,17 +17,51 @@ numb = "1","2","3","4","5","6","7","8","9","0"
 lett = "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"
 #Take Password Character lenght
 pcl = int(input("Please write the number of characters to be:"))
-pcl = pcl/2
 #Loop 
 while(True):
    if(nr == pcl):
-      print(password)
-      break
-   #True or false
-   tf = random.random()
-   #Password Character lenght
+    break
+   #True or false before (a ,a ) after (A,a)
+   tf = random.getrandbits(1)
+   # Lower case letter and numbers
    if(tf <= 0.5):
-    password = numb[random.randrange(0,9)] + lett[random.randrange(0,25)] + password
+     #random a bool 1 or 0
+     rgt = random.getrandbits(1)
+     #If the bool we randomly picked is less than 0.5, do this.
+     if(rgt <= 0.5):
+       password = numb[random.randrange(0,9)] + password
+     #If the bool we get randomly is greater than 0.5, do this
+     elif(rgt >= 0.5):
+        password = lett[random.randrange(0,25)] + password
+   # Uppercase letter and numbers
    else:
-    password = numb[random.randrange(0,9)] + lett[random.randrange(0,25)].upper() + password
+      #random a bool 1 or 0
+      rgt = random.getrandbits(1)
+      #If the bool we randomly picked is less than 0.5, do this.
+      if(rgt <= 0.5):
+        password = numb[random.randrange(0,9)] + password
+      #If the bool we get randomly is greater than 0.5, do this
+      elif(rgt >= 0.5):
+        password = lett[random.randrange(0,25)].upper() + password
    nr = nr + 1
+#Decor
+print("_" * pcl)
+#Write consol the password
+print(password)
+#Decor
+print("_" * pcl)
+save = input("Save your password in the text document? \n yes/no :")
+if(save == "yes"):
+   #take date
+   date = time.localtime()
+   #create name
+   file_name = str("YourPasswordIsHere ") + str(date.tm_year) + str(date.tm_mon) + str(date.tm_mday) + str(date.tm_min) + str(".txt")
+   #open file
+   file = open(file_name , "w")
+   #Write file
+   file.write(password)
+   file.close()
+else:
+  input("Press enter for exit.")
+#developer: ilyas YILDIZ
+#STORM Studio Bilişim Teknolojileri ürünüdür.
